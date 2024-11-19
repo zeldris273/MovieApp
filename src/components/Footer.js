f2a966daf0f5c8fceb3ac4ef518a4b118a3c8078
zeldris273 <113657396+zeldris273@users.noreply.github.com>
2024-11-19 21:51:12 +0700

    Thêm Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+   <footer className='text-center bg-neutral-600 bg-opacity-25 text-neutral-400 py-2'>
+    <div className='flex items-center justify-center gap-4'>
+      <Link to="/">About</Link>
+      <Link to="/">Contact</Link>
     </div>
+    <p className='text-sm'>Created by Nvh</p>
+   </footer>
   )
 }
 
